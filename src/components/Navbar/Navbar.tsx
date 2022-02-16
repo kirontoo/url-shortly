535f996422ae8faa090d84ade7ddcf239aa940ed
Amy Dang <kirontoo@gmail.com>
2022-02-16 11:35:18 -0800

    Fix navbar styling on mobile

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import './Navbar.scss';
 import  logo from 'logo.svg';
 
 export function Navbar() {
-  let [ navbarToggled, setNavbarToggled ] = useState< boolean >( true );
+  let [ navbarToggled, setNavbarToggled ] = useState<boolean>( true );
 
   useEffect( () => {
     // check if user is viewing on a mobile device
@@ -22,32 +22,29 @@ export function Navbar() {
   return (
     <nav className="navbar">
 
-      <div className="navbar__logo-container">
+      <div className="navbar__main">
         <img src={ logo } alt="" className="navbar__logo"/>
 
-        <a onClick={ toggleNavbar } href="#" className="navbar__media-toggle">
+        <a href="#" className="navbar__media-toggle">
           <span className="bar"></span>
           <span className="bar"></span>
           <span className="bar"></span>
         </a>
       </div>
 
-      <div style={{ display: navbarToggled ? 'flex' : 'none' }}  className="navbar__links-container">
-        <div className="navbar__header">
-          <ul className="navbar__links">
-            <li className="navbar__link"><a href="#">Features</a></li>
-            <li className="navbar__link"><a href="#">Pricing</a></li>
-            <li className="navbar__link"><a href="#">Resources</a></li>
-          </ul>
-        </div>
+      <div className="navbar__links-container">
+        <ul className="navbar__links navbar__header">
+          <li className="navbar__link"><a href="#">Features</a></li>
+          <li className="navbar__link"><a href="#">Pricing</a></li>
+          <li className="navbar__link"><a href="#">Resources</a></li>
+        </ul>
 
-        <span className="navbar__separator"></span>
-        <div className="navbar__footer">
-          <ul className="navbar__account-links">
-            <li className="navbar__account-link"><a href="#">Login</a></li>
-            <li className="navbar__account-link"><a href="#">Sign up</a></li>
-          </ul> 
-        </div>
+        <hr className="navbar__separator"/>
+
+        <ul className="navbar__links navbar__footer">
+          <li className="navbar__link"><a href="#">Login</a></li>
+          <li className="navbar__link"><a href="#">Sign up</a></li>
+        </ul> 
       </div>
 
     </nav>
